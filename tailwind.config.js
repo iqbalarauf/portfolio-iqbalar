7b0911357de3f11d8376d2691f7e08003fd783fe
Iqbal Abdul Ra'uf <iqbalar@student.telkomuniversity.ac.id>
2023-10-15 14:21:51 +0700

    Add about and skills (not final)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@
 module.exports = {
   content: ['index.html'],
   theme: {
+    container:{
+      center: true,
+      padding: '2rem',
+    },
     extend: {
       colors: {
         primary: '#10b981',
         dark: "#1e293b",
+      },
+      screens: {
+        '2xl': '1320px',
       }
     },
   },
